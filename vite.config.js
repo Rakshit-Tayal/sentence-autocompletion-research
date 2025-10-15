7b8bf1d5ded2760b180b8e0e92c31e68eb8bd231
Rakshit-Tayal <rakshittayal15@gmail.com>
2025-10-15 15:42:47 +0000

    fix: Final deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This tells Vite the correct path for the live site's assets.
-  base: "/sentence-autocompletion-research/", 
+  
+ 
 })
